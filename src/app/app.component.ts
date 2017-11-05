89f8b647c5cfd2e0643d92891c57e21c3d5f119d
Aravind <aravind.fz@gmail.com>
2017-11-05 12:32:00 +0530

    setup-files

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <main>
+    <h1>Hello from Angular App with Webpack</h1>
+  
+    
+  </main>`
+})
+export class AppComponent { }
